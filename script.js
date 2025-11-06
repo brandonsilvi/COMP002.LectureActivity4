e6161185799a99e3f30a417da2e77fa4de7b12be
Brandon Silvi <brandon.silvi@gmail.com>
2025-11-05 21:38:57 -0800

    finished exercise using push, pop, and creating a string and returning the length in the console log

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ console.log(shoeSizes[4]);
 *is in he first slot and 15 is in our final slot */
 
 function iterate(array) {
-    for(let i =0; i < array.length; i++){
-        console.log(`shoeSizes[${i}]`);
+    for (let i = 0; i < array.length; i++) {
+        console.log(`array[${i}]= ${array[i]}`);
     }
 }
+iterate(shoeSizes);
+shoeSizes.push(7);
+shoeSizes.push(11);
+shoeSizes.push(16);
+shoeSizes.push(9);
+shoeSizes.push(12);
+iterate(shoeSizes);
+//these pushes add values (7, 11, 16, 9, 12) to the end of the array
+shoeSizes.pop();
+shoeSizes.pop();
+shoeSizes.pop();
+iterate(shoeSizes);
+//the contents of the array are now (5, 8, 10, 13, 15, 7, 11)
+
+let myString = "My shoe size is 14 and its a pain finding shoes in person"
+console.log("String length:", myString.length);
